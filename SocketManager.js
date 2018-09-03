d0b4cbfc112be7fa30624ed282f1eba0704779ff
Vicky Banh <samsokhua1@gmail.com>
2018-09-03 18:51:50 -0400

    set and delete users that are typing

--- SocketManager.js
+++ SocketManager.js
@@ -1,13 +1,28 @@
+const TYPING = 'TYPING';
+const SEND = 'SEND';
+const NEW_MESSAGE = 'NEW_MESSAGE';
+const REPLYING = 'REPLYING';
+const ADD_TYPING_USER = 'ADD_TYPING_USER';
+const REMOVE_TYPING_USER = 'REMOVE_TYPING_USER';
+
+
 const io = require('./index').io;
 
 module.exports = function (socket) {
   console.log("New client connected with socketId:", socket.id);
 
-  socket.on("New_Message", message => {
-    console.log("Received message: ", message.text);
-    socket.broadcast.emit("New_Message", message);
+  socket.on(NEW_MESSAGE, message => {
+    console.log(`ID: ${socket.id} Received message: ${message.text}`);
+    socket.broadcast.emit(NEW_MESSAGE, message);
   });
 
+  socket.on(ADD_TYPING_USER, userID => {
+    socket.broadcast.emit(ADD_TYPING_USER, userID);
+  });
+
+  socket.on(REMOVE_TYPING_USER, userID => {
+    socket.broadcast.emit(REMOVE_TYPING_USER, userID);
+  });
 
   socket.on("disconnect", () => console.log("Client disconnected"));
 }
